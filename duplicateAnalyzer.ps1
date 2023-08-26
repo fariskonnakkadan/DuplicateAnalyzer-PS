0e01e77640e30c785202d0de053f23e68503001b
Faris <36856116+fariskonnakkadan@users.noreply.github.com>
2023-08-26 21:10:44 +0530

    Add files via upload

--- duplicateAnalyzer.ps1
+++ duplicateAnalyzer.ps1
@@ -1,5 +1,7 @@
 param (
-    [string]$folderPath
+    [string]$folderPath,
+    [string]$fileType,
+    [switch]$deleteDuplicates  # New switch parameter to enable deleting duplicate files
 )
 
 # Validate folder path
@@ -23,32 +25,67 @@ function Calculate-FileHash {
     return $hashString
 }
 
-# Recursively compute hashes of all files
-$hashes = @{}
-Get-ChildItem -Path $folderPath -File -Recurse | ForEach-Object {
-    $filePath = $_.FullName
-    $hash = Calculate-FileHash -filePath $filePath
-    $hashes[$hash] += @($filePath)
-}
+$files = Get-ChildItem -Path $folderPath -File -Recurse -Filter $fileType
+$totalFiles = $files.Count
+$currentFileIndex = 0
 
-# Identify and display duplicates
-$table = @{}
-foreach ($hash in $hashes.Keys) {
-    $files = $hashes[$hash]
-    if ($files.Count -gt 1) {
-        $table[$hash] = $files
+$hashes = @{}
+$filesToDelete = @()  # Initialize an array to store files to be deleted
+
+foreach ($file in $files) {
+    $filePath = $file.FullName
+    
+    $currentFileIndex++
+    $percentComplete = ($currentFileIndex / $totalFiles) * 100
+    Write-Progress -Activity "Calculating Hashes" -Status "Processing: $($currentFileIndex) of $($totalFiles)" -PercentComplete $percentComplete
+
+    $hash = Calculate-FileHash -filePath $filePath
+    
+    if ($hashes.ContainsKey($hash)) {
+        $hashes[$hash] += @($filePath)
+    } else {
+        $hashes[$hash] = @($filePath)
     }
 }
 
-if ($table.Count -gt 0) {
-    Write-Host "Duplicate Files:"
-    foreach ($hash in $table.Keys) {
-        Write-Host ("Hash: $hash")
-        $table[$hash] | ForEach-Object {
-            Write-Host ("  $_")
+Write-Progress -Completed
+
+if ($deleteDuplicates) {
+    foreach ($hash in $hashes.Keys) {
+        $filesList = $hashes[$hash]
+        if ($filesList.Count -gt 1) {
+            # Keep the first file (original) and delete the rest
+            for ($i = 1; $i -lt $filesList.Count; $i++) {
+                $fileToDelete = $filesList[$i]
+                $filesToDelete += $fileToDelete
+            }
         }
-        Write-Host ("=" * 40)
+    }
+
+    foreach ($fileToDelete in $filesToDelete) {
+        Remove-Item -Path $fileToDelete -Force
+        Write-Host "Deleted: $fileToDelete"
     }
 } else {
-    Write-Host "No duplicate files found."
+    # Identify and display duplicates
+    $table = @{}
+    foreach ($hash in $hashes.Keys) {
+        $filesList = $hashes[$hash]
+        if ($filesList.Count -gt 1) {
+            $table[$hash] = $filesList
+        }
+    }
+
+    if ($table.Count -gt 0) {
+        Write-Host "Duplicate Files:"
+        foreach ($hash in $table.Keys) {
+            Write-Host ("Hash: $hash")
+            $table[$hash] | ForEach-Object {
+                Write-Host ("  $_")
+            }
+            Write-Host ("=" * 40)
+        }
+    } else {
+        Write-Host "No duplicate files found."
+    }
 }
